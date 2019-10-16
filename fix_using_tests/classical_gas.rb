665d2e3a861b3ada935c4d51c9d9f9f193f6c1cd
Griffin Poole <griffinpoole5@gmail.com>
2019-10-16 03:28:53 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -10,7 +10,3 @@ class GasStation
 seashore_shell = GasStation.new("The Seashore Shell", 40)
 dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
 end
-
-petrol_petes = GasStation.new("Petrol Pete's", 50)
-seashore_shell = GasStation.new("The Seashore Shell", 40)
-dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
