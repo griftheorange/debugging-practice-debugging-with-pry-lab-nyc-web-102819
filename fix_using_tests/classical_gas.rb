e361a966061efd0ceb892344ff6ecdfdc3428843
Griffin Poole <griffinpoole5@gmail.com>
2019-10-16 03:28:49 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -6,7 +6,9 @@ class GasStation
     @brand = brand
     @unleaded_price = unleaded_price
   end
-  
+  petrol_petes = GasStation.new("Petrol Pete's", 50)
+seashore_shell = GasStation.new("The Seashore Shell", 40)
+dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
 end
 
 petrol_petes = GasStation.new("Petrol Pete's", 50)
